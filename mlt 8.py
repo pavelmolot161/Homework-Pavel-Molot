6564ae27cb87d5f603a42086d5728d1efdf6dbe4
pavelmolot161 <pavel.molot.161@yandex.ru>
2024-06-24 16:22:48 +0300

    Add files via upload

---
+++ mlt 8.py
@@ -0,0 +1,20 @@
+   # МЕТОДЫ СТРОК #
+#name = input('Введите Ваше имя: ')
+#current_year = 2024
+#date_of_birth = input("В каком году Вы родились ? ")
+#age = current_year - int(date_of_birth)
+#print("Здравствуйте,", name)
+#print("В этом году Вам", age, "года")
+#print('Привет я строка в нижнем регистре' .upper().lower())
+#printC('Привет я строка в нижнем регистре' .replace('Привет', 'Пока'))
+#print(('Привет я строка в нижнем регистре').replace(' ', '$'))
+
+  # Homework 4 #
+my_string = input("Самое главное в жизни, это семья.")
+print("Самое главное в жизни, это семья.".upper())
+print("Самое главное в жизни, это семья.".lower())
+print(("Самое главное в жизни, это семья.").replace(" ", ""))
+my_string = "Самое главное в жизни, это семья."
+print(my_string[0])
+print(my_string[-1])
+
