c8a514d7d9a287b4be9fe0a838367ed23512159e
pavelmolot161 <pavel.molot.161@yandex.ru>
2024-06-24 16:25:09 +0300

    Add files via upload

--- mlt 8.py
+++ mlt 8.py
@@ -1,13 +1,4 @@
-   # МЕТОДЫ СТРОК #
-#name = input('Введите Ваше имя: ')
-#current_year = 2024
-#date_of_birth = input("В каком году Вы родились ? ")
-#age = current_year - int(date_of_birth)
-#print("Здравствуйте,", name)
-#print("В этом году Вам", age, "года")
-#print('Привет я строка в нижнем регистре' .upper().lower())
-#printC('Привет я строка в нижнем регистре' .replace('Привет', 'Пока'))
-#print(('Привет я строка в нижнем регистре').replace(' ', '$'))
+
 
   # Homework 4 #
 my_string = input("Самое главное в жизни, это семья.")
