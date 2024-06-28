cc9524b229e996598aab56974a0d64494b3a7596
pavelmolot161 <pavel.molot.161@yandex.ru>
2024-06-28 18:07:29 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,28 @@
+
+# ДЗ Условная конструкция. Операторы if, elif, else.
+
+first = 6
+second = 7
+third = 3
+if first == second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+elif first != second or second != third or first != third:
+    print(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
