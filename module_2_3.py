8298bbdea30fa4420034e2dbb30c5d325c1c4d1b
pavelmolot161 <pavel.molot.161@yandex.ru>
2024-07-03 17:14:52 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,33 @@
+
+   # ДЗ 2_3 #
+
+mu_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+asd = 0
+while asd <= len(mu_list):
+    if mu_list[asd] < 0:
+        break
+    else:
+        if mu_list[asd] > 0:
+            print(mu_list[asd])
+    asd = asd + 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
