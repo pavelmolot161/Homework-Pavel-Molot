33bc69271b2ea330a934e6d5b040a47ba89fc80e
pavelmolot161 <pavel.molot.161@yandex.ru>
2024-07-18 06:27:23 +0300

    Add files via upload

---
+++ true_math.py
@@ -0,0 +1,25 @@
+
+
+from math import inf
+def divide_1(first, second):
+    if second == 0:
+        # print(inf)
+        return inf
+    elif second != 0:
+        n = first / second
+        # print(n)
+        return n
+
+
+
+
+
+
+
+
+
+
+
+
+
+
