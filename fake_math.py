aa8f666a901f89705da937dca33ede80adb97633
pavelmolot161 <pavel.molot.161@yandex.ru>
2024-07-18 06:26:18 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,15 @@
+
+
+def divide(first, second):
+    if second == 0:
+        # print("Ошибка")
+        return "Ошибка"
+    elif second != 0:
+        n = first / second
+        # print(n)
+        return n
+
+
+
+
+
