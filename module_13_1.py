8b9595fd333d064981c90d83c80ffcf652fa2c8a
pavelmolot161 <pavel.molot.161@yandex.ru>
2024-10-16 21:11:13 +0300

    Add files via upload

---
+++ module_13_1.py
@@ -0,0 +1,27 @@
+
+
+import time
+import asyncio
+
+async def start_strongman(name, power):
+    print(f'Силач {name} начал соревнования.')
+    for number_bol in range(1, 6):
+        await asyncio.sleep(power)
+        print(f'Силач {name} поднял {number_bol}-й шар.')
+    print(f'Силач {name} закончил соревнования.')
+
+async def start_tournament():
+    print('Старт')
+    strongman1 = asyncio.create_task(start_strongman('Pasha', 5))
+    strongman2 = asyncio.create_task(start_strongman('Denis', 4))
+    strongman3 = asyncio.create_task(start_strongman('Apollon', 3))
+    await strongman1
+    await strongman2
+    await strongman3
+
+    print('Финиш')
+start = time.time()
+asyncio.run(start_tournament())
+finish = time.time()
+print(f'Рабочее время = {round(finish - start, 10)} секунд')
+
